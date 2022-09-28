65573cfe524679f499a10cf8eb0673736f58807f
Cauã Roberto <107198495+Caua-Roberto@users.noreply.github.com>
2022-09-28 10:36:50 -0300

    Add files via upload

---
+++ etapas03.js
@@ -0,0 +1,93 @@
+let etapas = [
+    {
+        titulo: 'DEP. FEDERAL',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '3456',
+                nome: 'Rodrigo Faro',
+                partido: 'Record TV',
+                fotos: [
+                    {url:'rodrigo.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '2412',
+                nome: 'Celso Portioli',
+                partido: 'SBT',
+                fotos: [
+                    { url: '12345.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '6789',
+                nome: 'Faustão',
+                partido: 'Band',
+                fotos: [
+                    {url: '87654.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '92001',
+                nome: 'Fábio',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '92001.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'DEP. ESTADUAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '68',
+                nome: 'Lívia',
+                partido: 'ABC',
+                vice: 'Sebastião',
+                fotos: [
+                    {url: '68.jpg', legenda: 'Prefeito'},
+                    {url: '68_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '42',
+                nome: 'Manuel',
+                partido: 'DEFG',
+                vice: 'Martin',
+                fotos: [
+                    {url: '42.jpg', legenda: 'Prefeito'},
+                    {url: '42_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '68',
+                nome: 'Lívia',
+                partido: 'ABC',
+                vice: 'Sebastião',
+                fotos: [
+                    {url: '68.jpg', legenda: 'Prefeito'},
+                    {url: '68_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '42',
+                nome: 'Manuel',
+                partido: 'DEFG',
+                vice: 'Martin',
+                fotos: [
+                    {url: '42.jpg', legenda: 'Prefeito'},
+                    {url: '42_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            }
+        ]
+    }
+    
+]
